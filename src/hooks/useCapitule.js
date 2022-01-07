5d6b33f896691f9f6526fa1bd6f55425aa4de71f
SolidLoL <solid_nerv@hotmail.com>
2022-01-06 23:37:33 -0600

    Videos Scss Etc ...

---
+++ src/hooks/useCapitule.js
@@ -0,0 +1,23 @@
+import { useEffect, useState} from "react";
+import axios from "axios";
+
+
+const useAnimeInfo = (id,episode) => {
+    const [anime, setAnime] = useState([]);
+
+    
+    useEffect(async () => {
+        const API =`https://api.aniapi.com/v1/episode?anime_id=${id}&number=${episode}`;
+
+        const responseAPI = await axios(API);
+        //Asign data to response
+        const {documents} = responseAPI.data?.data;
+        
+        //setAnime(response.data);
+        setAnime(documents);
+      }, []);
+      return anime;
+    
+}
+
+export default  useAnimeInfo ;
