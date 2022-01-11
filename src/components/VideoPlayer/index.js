b3c7fd5f3f67d93ff0e7b7568af4e6c69fa9995b
SolidLoL <solid_nerv@hotmail.com>
2022-01-10 23:06:09 -0600

    VideoPlayer interface action button

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,29 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import "./style.scss";
 
 export const VideoPlayer = ({ video }) => {
-    const [play, setplay] = useState(false)
-    const divStyle = {
+    /* const divStyle = {
         zIndex: 9,
         top: 0,
-        position: "fixed",
+        width: "100vh",
+        height: "100vw",
+        transformOrigin: "top",
+        transform: "rotate(90deg)",
+        position: "absolute",
+        background: "black"
+    }; */
 
-    };
+    const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = useState({})
+    const [orientation, setorientation] = useState(0)
+    const [play, setplay] = useState(false);
+    const navigate = useNavigate();
+    const playerRef = useRef(null);
+    const handleSeekMouseDown = e => {
+        setplayed({ seeking: true })
+    }
+    const handleSeekChange = e => {
+        setplayed({ played: parseFloat(e.target.value) })
+    }
+    const handleSeekMouseUp = e => {
+        setplayed({ seeking: false })
+        this.player.seekTo(parseFloat(e.target.value))
+    }
+    /* cONTROLLS */
+    const ControllzVideoPlayer = ({ props, player }) => {
+        console.log(player)
 
-    const ControllzVideoPlayer = ({ props }) => {
-        console.log(props)
-        const { video, title, number } = props;
+        const handlePlay = () => {
+            setplay(!play);
+        }
+
+        const { title, number } = props;
         return (
             <div className='controlls d-flex flex-column w-100 position-absolute'>
-                <div className='duration'></div>
-                <div className='play-controlls d-flex flex-row justify-content-around'>
-                    <div className='capitule d-flex flex-row justify-content-evenly align-items-center'>
-                    <div className='time-lapse'>00:00</div>
-                    <div className='episode-info'>
-                        <div className='name'>{title ? title : "titulo no disponible"}</div>
-                        <div className='number'>{`capitulo ${number}`}</div>
-                    </div>
+                <div className='duration'>
+                    <input
+                        type='range' min={0} max={0.999999} step='any'
+                        value={played}
+                        onMouseDown={handleSeekMouseDown}
+                        onChange={handleSeekChange}
+                        onMouseUp={handleSeekMouseUp}
+                    />
+                </div>
+                <div className='play-controlls d-flex flex-row '>
+                    <div className='capitule d-flex flex-row  align-items-center'>
+                        <div className='time-lapse'>00:00</div>
+                        <div className='episode-info'>
+                            <div className='name'>{title ? title : "titulo no disponible"}</div>
+                            <div className='number'>{`capitulo ${number}`}</div>
+                        </div>
                     </div>
                     <div className='buttons d-flex flex-row justify-content-between align-items-center'>
                         <div className='re-init d-none d-lg-block'>
@@ -36,7 +69,7 @@ export const VideoPlayer = ({ video }) => {
                                 fast_rewind
                             </span>
                         </div>
-                        <div className='play'>
+                        <div className='play' onClick={handlePlay}>
                             <span className="material-icons">
                                 {play ? `pause` : `play_arrow`}
                             </span>
@@ -72,18 +105,31 @@ export const VideoPlayer = ({ video }) => {
         )
     }
 
-
+    useEffect(() => {
+        window.addEventListener("orientationchange", function () {
+            const worient = screen.orientation;
+            if (worient.angle != orientation) {
+                setorientation(worient.angle)
+            }
+        }, false);
+    }, [])
 
     console.log("----------------------> Video <---------------")
-    console.log(video)
-    console.log(Object.entries(ReactPlayer))
+    console.log(playerRef)
     console.log("----------------------> Video <---------------")
-    const handlePlay = () => {
-        setplay(!play);
-    }
+
     return (
         <div className='player-wraper'>
-            <ReactPlayer url={`${video.video}`} width={"100vw"} height={"100vh"} style={divStyle} playing={play} />
+            <div className='back position-absolute w-100' >
+                <div className='d-flex flex-row justify-content-start align-items-center p-3 '
+                    onClick={() => { navigate(-1) }}>
+                    <span className="material-icons">
+                        arrow_back
+                    </span>
+                    <span>Back</span>
+                </div>
+            </div>
+            <ReactPlayer ref={playerRef} className="player-react" url={`${video.video}`} width={orientation === 0 ? `100vw` : `100vh`} height={orientation === 0 ? `100vh` : `100vw`} playing={play} />
             <ControllzVideoPlayer props={video} />
         </div>
     );
